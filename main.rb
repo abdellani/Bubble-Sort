9499d1bb15278e774ff2f6bcf5f98c5687c7a983
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-06 02:02:53 +0530

    Fix rubycop errors

--- main.rb
+++ main.rb
@@ -1,48 +1,41 @@
+# frozen_string_literal: true
+
 # Define an array
-array=[4,3,78,2,0,2]
+array = [4, 3, 78, 2, 0, 2]
 
 # 1. Assignment 1
 def bubble_sort array
-  while  true
-    swapped=false
-    (array.length-1).times do |x|
-      if(array[x]> array[x+1])
-        tmp=array[x]
-        array[x]=array[x+1]
-        array[x+1]=tmp
-        swapped=true
+  swapped = true
+  while swapped
+    swapped = false
+    (array.length - 1).times do |x|
+      if array[x] > array[x + 1] 
+        tmp = array[x]
+        array[x] = array[x + 1]
+        array[x + 1] = tmp
+        swapped = true
       end
-    end 
-    break unless swapped
+    end
   end
   array
 end
 
-
 puts bubble_sort array
-
 
 # 2. Assignment 2 with Block
 def bubble_sort_by(array)
-  while true
+  swapped = true
+  while swapped
     swapped = false
-    (array.length - 1).times do |i| 
+    (array.length - 1).times do |i|
       if yield(array[i], array[i + 1])
-        tmp=array[i]
+        tmp = array[i]
         array[i] = array[i + 1]
         array[i + 1] = tmp
         swapped = true
       end
     end
-    break unless swapped
   end
   array
 end
- 
-  
-puts bubble_sort_by([4,3,78,2,0,2]) { |left, right| 
-  left > right
-}
-
-
-
+puts bubble_sort_by([4, 3, 78, 2, 0, 2]) { |left, right| left > right }
